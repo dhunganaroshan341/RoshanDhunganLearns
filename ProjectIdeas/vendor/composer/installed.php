24a762e1e64505873603329609124d2ee748b87e
Roshan Dhungana <dhunganaroshan341@gmail.com>
2026-04-05 14:08:36 +0545

    created route comannds

---
+++ ProjectIdeas/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '78af805cdff18c2027fb5f78cb33606a65134eac',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '78af805cdff18c2027fb5f78cb33606a65134eac',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'roshan-dhungana/route-commands' => array(
+            'pretty_version' => 'v1.0.0',
+            'version' => '1.0.0.0',
+            'reference' => 'd6d65a84f7214c0970de2235cf8e789fcc8c158e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../roshan-dhungana/route-commands',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
